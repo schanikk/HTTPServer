94c18c5a94c3c9c7600b5c12d0be9e54d43eab0c
Till <till.nikolaus.schaland@web.de>
2022-09-11 19:13:41 +0200

    - enabled Multiple Connections using Threading

---
+++ src/main/java/com/Haizen/httpserver/core/HttpConnectionWorkerThread.java
@@ -0,0 +1,68 @@
+package com.Haizen.httpserver.core;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+
+public class HttpConnectionWorkerThread extends Thread {
+    private final static Logger LOGGER = LoggerFactory.getLogger(HttpConnectionWorkerThread.class);
+    private Socket socket;
+
+    public HttpConnectionWorkerThread(Socket socket) {
+        this.socket = socket;
+    }
+
+    @Override
+    public void run() {
+        InputStream inputStream = null;
+        OutputStream outputStream = null;
+        try {
+
+            inputStream = socket.getInputStream();
+            outputStream = socket.getOutputStream();
+
+            String html = "<html><head><title>Simple Java HTTP Server</title></head><body><h1>This page was served using Simple HTTP Server </h1></body></html>";
+
+            final String CRLF = "\n\r"; //13, 10
+
+            String response =
+                    "HTTP/1.1 200 OK" + CRLF + //Status Line : HTTP VERSION RESPONSE_CODE RESPONSE_MESSAGE
+                            "Content-Length: " + html.getBytes().length + CRLF + //HEADER
+                            CRLF +
+                            html +
+                            CRLF + CRLF;
+
+            outputStream.write(response.getBytes());
+
+            LOGGER.info("Connection processed Succesfully");
+        } catch (IOException e) {
+            LOGGER.info("Problem with communication", e);
+            // TODO Handle Later
+        } finally {
+
+            if (inputStream != null) {
+                try {
+                    inputStream.close();
+                } catch (IOException e) {
+                }
+            }
+            if (outputStream != null) {
+                try {
+                    outputStream.close();
+                } catch (IOException e) {
+                }
+            }
+            if (socket != null) {
+                try {
+                    socket.close();
+                } catch (IOException e) {
+                }
+            }
+        }
+
+    }
+}
